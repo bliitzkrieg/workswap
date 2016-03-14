1f4c8ad86449c5eead2c375afbe4364326d48872
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-03-13 21:14:28 -0400

    Fixed usercheck bug

--- client/modules/core/components/main-layout/main.jsx
+++ client/modules/core/components/main-layout/main.jsx
@@ -3,9 +3,10 @@ import Header from '../header/header.jsx';
 
 class MainLayout extends React.Component {
     render() {
+        const username =  Meteor.user() ? Meteor.user().username : null; // This should be abstracted out. State should be handled in a container
         return (
             <div>
-                <Header username={ Meteor.user().username }/>
+                <Header username={ username }/>
                 <main className="container">
                     { this.props.content }
                 </main>
