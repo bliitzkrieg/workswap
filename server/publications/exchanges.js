e0ce255494b66c73f2d4ce81980f79c04e92b73c
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-03-15 00:39:35 -0400

    limited results to 15 and sorted in descending order

--- server/publications/exchanges.js
+++ server/publications/exchanges.js
@@ -4,6 +4,9 @@ import { check } from 'meteor/check';
 
 export default function () {
     Meteor.publish('exchanges.list', function () {
-        return Exchanges.find();
+        return Exchanges.find({}, {
+            limit: 15,
+            sort: {createdAt: -1}
+        });
     });
 }
