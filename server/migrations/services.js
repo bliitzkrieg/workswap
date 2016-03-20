9a73c51d6f409398d6cefd79c72b4b3b724436c9
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-03-19 22:21:22 -0400

    Started migration

--- server/migrations/services.js
+++ server/migrations/services.js
@@ -1,6 +1,135 @@
 import { Services } from '/lib/collections';
 import { Meteor } from 'meteor/meteor';
 
+const ServiceData = [
+    {
+        name: 'Web Developer'
+    },
+    {
+        name: 'Web Designer'
+    },
+    {
+        name: 'Graphic Designer'
+    },
+    {
+        name: 'Painter'
+    },
+    {
+        name: 'Voice Actor'
+    },
+    {
+        name: 'Actor'
+    },
+    {
+        name: 'Videographer'
+    },
+    {
+        name: 'Photographer'
+    },
+    {
+        name: 'Hypeman'
+    },
+    {
+        name: 'Musician'
+    },
+    {
+        name: 'Music Producer'
+    },
+    {
+        name: 'Songwriter'
+    },
+    {
+        name: 'Marketer'
+    },
+    {
+        name: 'Business Consultant'
+    },
+    {
+        name: 'Video Editor'
+    },
+    {
+        name: 'Special Effects Editor'
+    },
+    {
+        name: 'Carpenter'
+    },
+    {
+        name: 'Plumber'
+    },
+    {
+        name: 'Electrician'
+    },
+    {
+        name: 'Computer Technician'
+    },
+    {
+        name: 'IT'
+    },
+    {
+        name: 'Architect'
+    },
+    {
+        name: 'Interior Designer'
+    },
+    {
+        name: 'Video Game Designer'
+    },
+    {
+        name: 'Public Relations'
+    },
+    {
+        name: 'Accountant'
+    },
+    {
+        name: 'SEO Specialist'
+    },
+    {
+        name: 'Quality Assurance'
+    },
+    {
+        name: 'Movers'
+    },
+    {
+        name: 'Baker'
+    },
+    {
+        name: 'Chef'
+    },
+    {
+        name: 'Comedian'
+    },
+    {
+        name: 'Copywriter'
+    },
+    {
+        name: 'Personal Trainer'
+    },
+    {
+        name: 'Animal Walker'
+    },
+    {
+        name: 'Dance Instructor'
+    },
+    {
+        name: 'Mechanic'
+    }
+];
+
 export default function () {
+
+    Migrations.add({
+        version: 1,
+        up: function() {
+
+            ServiceData.forEach(function(item){
+                console.log(item.name); // Do Insert if no results in table.
+            })
+
+        }
+        });
+
+    Meteor.startup(function () {
+        Migrations.migrateTo('latest');
+    });
 
 }
